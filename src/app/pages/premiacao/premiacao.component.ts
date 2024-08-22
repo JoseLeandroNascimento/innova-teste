538febcc9c8ce3a97c18411a3427b0fb1c6cc501
02705534288 <leandronascimento@lampp-it.com.br>
2024-08-22 11:50:28 -0500

    criando componentes de etapa

---
+++ src/app/pages/premiacao/premiacao.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-premiacao',
+  standalone: true,
+  imports: [],
+  templateUrl: './premiacao.component.html',
+  styleUrl: './premiacao.component.scss'
+})
+export class PremiacaoComponent {
+
+}
